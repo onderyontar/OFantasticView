f49bbd2ef4b690b5516378423b16d5d1f2b1a935
Önder YONTAR <onder.yontar@vizyoneks.com.tr>
2017-07-13 10:29:39 +0300

    add

---
+++ OFantasticView/FanstasticView.swift
@@ -0,0 +1,13 @@
+//
+//  FanstasticView.swift
+//  OFantasticView
+//
+//  Created by Önder YONTAR on 13/07/2017.
+//  Copyright © 2017 Önder Yontar. All rights reserved.
+//
+
+import UIKit
+
+class FantasticView: UIView {
+    
+}
